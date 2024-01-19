7a20bcea84008d50627681b34e7969e9c75389e7
Cristian <cristian.pozo@gmail.com>
2024-01-19 19:35:01 -0300

    Se corrigen errores en variables, alert y otros

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,58 +2,83 @@ import { Button, Form } from "react-bootstrap"
 import { useState } from "react"
 
 const Formulario = ({ setAlert }) => {
-	const [name, setName] = useState("")
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-	const [repassword, setRePassword] = useState("")
-	const validateMail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
+    const [confPassword, setConfPassword] = useState("")
+    const validateMail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
 
     const validateForm = (e) => {
-		e.preventDefault()
-		if (password !== repassword) {
-			setAlert({
-				error: true,
-				msg: "Las contraseñas no coinciden !",
-				color: "warning",
-			})
-
+        e.preventDefault()
+        if (password !== confPassword) {
+            setAlert({
+                error: true,
+                msg: "Las contraseñas no coinciden !",
+                color: "warning",
+            })
+            return
         }
-
-//FALTA VALIDAR LOS INPUTS
-
-        return (
-            <>
-                <Form onSubmit={validateForm} noValidate>
-                    <Form.Control
-                        type="text"
-                        id="name"
-                        onChange={(e) => setName(e.target.value)}
-                        value={name}
-                        placeholder="Nombre"
-                    />
-                    <Form.Control
-                        type="email"
-                        id="email"
-                        onChange={(e) => setEmail(e.target.value)}
-                        value={email}
-                        placeholder="tuemail@ejemplo.com"
-                    />
-                    <Form.Control
-                        type="password"
-                        id="password"
-                        onChange={(e) => setPassword(e.target.value)}
-                        value={password}
-                        placeholder="Contraseña"
-                    />
-                    <Form.Control
-                        type="password"
-                        id="repassword"
-                        onChange={(e) => setRePassword(e.target.value)}
-                        value={repassword}
-                        placeholder="Confirma tu contraseña"
-                    />
-                    <Button type="submit" variant="success">Registrarse</Button>
-                </Form>
-            </>
-        )
+        if (name === '' || email === '' || password === '' || confPassword === '') {
+            setAlert({
+                error: true,
+                msg: "Completa todos los campos !",
+                color: "danger",
+            })
+            return
+        }
+        if (!validateMail.test(email)) {
+            setAlert({
+                error: true,
+                msg: "El email debe tener un formato válido !",
+                color: "danger",
+            })
+            return
+        }
+        setAlert({
+            error: false,
+            msg: "Cuenta creada existosamente!",
+            color: "success"
+        })
+        setName('')
+        setEmail('')
+        setPassword('')
+        setConfPassword('')
     }
-    export default Formulario
+
+    return (
+        <>
+            <Form onSubmit={validateForm} noValidate>
+                <Form.Control
+                    type="text"
+                    id="name"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                    placeholder="Nombre"
+                />
+                <Form.Control
+                    type="email"
+                    id="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    placeholder="tuemail@ejemplo.com"
+                />
+                <Form.Control
+                    type="password"
+                    id="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    placeholder="Contraseña"
+                />
+                <Form.Control
+                    type="password"
+                    id="ConfPassword"
+                    onChange={(e) => setConfPassword(e.target.value)}
+                    value={confPassword}
+                    placeholder="Confirma tu contraseña"
+                />
+                <Button type="submit" variant="success">Registrarse</Button>
+            </Form>
+        </>
+    )
+}
+export default Formulario
