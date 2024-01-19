48f229f2b54bdccf0ff6960289e1151bc701abf7
Cristian <cristian.pozo@gmail.com>
2024-01-19 11:15:24 -0300

    Se añade onSubmit para validar

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -15,3 +15,45 @@ const Formulario = ({ setAlert }) => {
 				msg: "Las contraseñas no coinciden !",
 				color: "warning",
 			})
+
+        }
+
+//FALTA VALIDAR LOS INPUTS
+
+        return (
+            <>
+                <Form onSubmit={validateForm} noValidate>
+                    <Form.Control
+                        type="text"
+                        id="name"
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                        placeholder="Nombre"
+                    />
+                    <Form.Control
+                        type="email"
+                        id="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        placeholder="tuemail@ejemplo.com"
+                    />
+                    <Form.Control
+                        type="password"
+                        id="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        placeholder="Contraseña"
+                    />
+                    <Form.Control
+                        type="password"
+                        id="repassword"
+                        onChange={(e) => setRePassword(e.target.value)}
+                        value={repassword}
+                        placeholder="Confirma tu contraseña"
+                    />
+                    <Button type="submit" variant="success">Registrarse</Button>
+                </Form>
+            </>
+        )
+    }
+    export default Formulario
